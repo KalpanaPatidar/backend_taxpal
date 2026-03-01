7a887bcb393d86131778d3eb6b09eb1e7cef3400
Pritisha Kumari <150917818+Pritisha-Kri@users.noreply.github.com>
2026-03-01 18:57:56 +0530

    Comment out email validations in User model
    Commented out email field validations in User model.

--- models/User.js
+++ models/User.js
@@ -13,11 +13,11 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: [true, 'Email is required'],
+    //required: [true, 'Email is required'],
     unique: true,
-    trim: true,
-    lowercase: true,
-    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+    //trim: true,
+    //lowercase: true,
+    //match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
   },
   password: {
     type: String,
